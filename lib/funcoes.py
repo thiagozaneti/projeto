f909949dd43868bf8db4d14c70466848849fc945
ThiagoZaneti <dotinha0203@gmail.com>
2024-03-19 15:38:27 -0300

    inicio situação de aprendizagem

---
+++ lib/funcoes.py
@@ -0,0 +1,14 @@
+def menu():
+    while True:
+        print("""
+        menu[1]
+        menu[1]
+        menu[1]
+        menu[1]
+""")
+        user_select = int(input("Digite a sua escolha: "))
+        if user_select == 1:
+            pass
+
+
+        
---
+++ main.py
@@ -0,0 +1,3 @@
+from lib.funcoes import menu
+if __name__ == "__name__":
+    menu()
